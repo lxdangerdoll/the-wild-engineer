10c0b40417e5c9915e850ff340c32a87fbf59bcc
Synapse Studios <lxdangerdoll@gmail.com>
2026-03-11 14:07:32 -0400

    inertial kermit. -Mercy 'Simply The Best' Danger <8>

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [react()],
+
+base: '/the-wild-engineer/',
+
+plugins: [
+
+react(),
+
+tailwindcss(),
+],
+
 })
